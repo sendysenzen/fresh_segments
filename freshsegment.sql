0fc94939cb421e36d6cb030346cbf63525cbc97d
Sendy Setiawan <sendy.senzen@gmail.com>
2022-08-06 12:11:57 +0200

    Update freshsegment.sql

--- freshsegment.sql
+++ freshsegment.sql
@@ -148,6 +148,127 @@ HAVING count(DISTINCT month_year) = 14
 
 -- B.2 Using this same total_months measure - calculate the cumulative percentage of all records starting at 14 months - 
 -- which total_months value passes the 90% cumulative percentage value?
--- key words: ALL RECORDS please note this
+-- key words: ALL RECORDS 
+
+WITH cte AS (
+SELECT
+    interest_id,
+    count(DISTINCT month_year) total_months
+FROM interest_metrics
+GROUP BY 1
+)
+SELECT 
+    total_months,
+    COUNT(total_months) count_interest_id,
+    ROUND(100*SUM(COUNT(total_months)) OVER(ORDER BY total_months DESC)/SUM(COUNT(total_months)) OVER(),2) AS cumm_percentage
+FROM cte
+GROUP BY 1
+ORDER BY 1 DESC
+
+-- 3. If we were to remove all interest_id values which are lower than the total_months value we found in the previous question - 
+-- how many total data points would we be removing?
+
+-- the question is unclear. 
+SELECT 
+    COUNT(*) 
+FROM interest_metrics
+WHERE interest_id IN (
+    SELECT
+        interest_id
+    FROM interest_metrics
+    GROUP BY 1
+    HAVING count(DISTINCT month_year) < 14
+    ) -- this is for example if we want to remove datapoint that dont have 14 months data. 
+
+-- 4. Does this decision make sense to remove these data points from a business perspective? 
+-- Use an example where there are all 14 months present to a removed interest example for your arguments - 
+-- think about what it means to have less months present from a segment perspective.
+
+-- in my opinion it depends on the type of analysis that we want to achieve. if consistency in data is required and that we 
+-- need to see the time series based data, we may want to exclude this. But in other many cases, these 'incomplete'
+-- monthly data may present different insights that can be required for the business. 
+
+
+-- 5. If we include all of our interests regardless of their counts - 
+-- how many unique interests are there for each month?
+
+SELECT
+    month_year, 
+    COUNT(DISTINCT interest_id) interest_per_month
+FROM interest_metrics
+GROUP BY 1
+    
+    
+-- C. SEGMENT ANALYSIS
+
+-- C.1 Using the complete dataset - which are the top 10 and bottom 10 interests which have the largest composition values 
+-- in any month_year? Only use the maximum composition value for each interest but you must keep the corresponding month_year
+
+-- key word: bottom 10 with largest & any month_year (its not bottom 10 with the smallest! I misunderstood this the first time)
+
+WITH cte_compo AS (
+SELECT
+    interest_id,
+    month_year,
+    composition,
+    RANK() OVER(PARTITION BY interest_id ORDER BY composition DESC) as rank_num
+FROM interest_metrics
+), top_10 AS ( 
+SELECT 
+    month_year,
+    interest_id,
+    composition
+FROM cte_compo
+WHERE rank_num = 1 
+ORDER BY composition DESC
+LIMIT 10
+), bottom_10 AS ( 
+SELECT 
+    month_year,
+    interest_id,
+    composition
+FROM cte_compo
+WHERE rank_num = 1 
+ORDER BY composition 
+LIMIT 10    
+)
+SELECT * FROM top_10
+UNION 
+SELECT * FROM bottom_10
+ORDER BY 3 DESC
+-- just join with interest_map to get the name
+
+
+-- C.2 Which 5 interests had the lowest average ranking value?
+SELECT 
+    t1.interest_id,
+    t2.interest_name,
+    ROUND(AVG(t1.ranking),2) avg_rank,
+    COUNT(t1.*) rec_count
+FROM interest_metrics t1
+INNER JOIN interest_map t2
+    ON t1.interest_id = t2.id
+GROUP BY 1,2
+ORDER BY 3
+LIMIT 5
+
+
+-- C.3 Which 5 interests had the largest standard deviation in their percentile_ranking value?
+SELECT 
+    t1.interest_id,
+    t2.interest_name,
+    ROUND(STDDEV(t1.percentile_ranking::NUMERIC),1) std_deviation,
+    max(t1.percentile_ranking),
+    min(t1.percentile_ranking),
+    COUNT(t1.*) rec_count
+FROM interest_metrics t1
+INNER JOIN interest_map t2
+    ON t1.interest_id = t2.id
+GROUP BY 1,2
+HAVING COUNT(t1.*) > 1 -- std dev with 1 record only provides null output
+ORDER BY 3 DESC
+LIMIT 5
+
+
 
 
